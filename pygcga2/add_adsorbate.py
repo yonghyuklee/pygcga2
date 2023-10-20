8fbbc0f89c00c6b045419fa9b8f596a4139df20a
Yonghyuk Lee <huckley90@gmail.com>
2023-10-20 16:40:10 -0700

    Edit H adsorbate length scale

--- pygcga2/add_adsorbate.py
+++ pygcga2/add_adsorbate.py
@@ -220,9 +220,9 @@ def add_multiple_H(surface, bond_range=None, max_trial=50):
         t = surface.copy()
         for i, n in enumerate(n_choose):
             l_min = min(get_bond_range(surface[n].symbol, "H"))
-            x = pos[n, 0] + np.random.normal(0, 1.2*l_min, 1)[0]
-            y = pos[n, 1] + np.random.normal(0, 1.2*l_min, 1)[0]
-            z = pos[n, 2] + np.abs(np.random.normal(0, 1.2*l_min, 1)[0])
+            x = pos[n, 0] + np.random.normal(0, 1.4*l_min, 1)[0]
+            y = pos[n, 1] + np.random.normal(0, 1.4*l_min, 1)[0]
+            z = pos[n, 2] + np.abs(np.random.normal(0, 1.4*l_min, 1)[0])
             t.append(Atom(symbol="H", position=[x, y, z], tag=(i+1)))
         inspect = checkatoms(t, bond_range)
         if inspect:
